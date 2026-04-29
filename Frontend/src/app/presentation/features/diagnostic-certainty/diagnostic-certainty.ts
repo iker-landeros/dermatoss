2ec20fa070838ee1526af74f9ea7af0dfd0a3df2
Iker <iker.landelao@gmail.com>
2026-04-29 02:48:18 -0600

    diagnostic certainty ready

--- Frontend/src/app/presentation/features/diagnostic-certainty/diagnostic-certainty.ts
+++ Frontend/src/app/presentation/features/diagnostic-certainty/diagnostic-certainty.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, computed, OnInit, signal } from '@angular/core';
+import { AnalysisService } from '../../../services/analysis.service';
 
 @Component({
   selector: 'app-diagnostic-certainty',
@@ -6,4 +7,39 @@ import { Component } from '@angular/core';
   templateUrl: './diagnostic-certainty.html',
   styleUrl: './diagnostic-certainty.css',
 })
-export class DiagnosticCertainty {}
+export class DiagnosticCertainty implements OnInit {
+
+  confidence = signal<number | null>(null);
+  prediction = signal<string | null>(null);
+
+  // Convert to percentage
+  confidencePercent = computed(() => {
+    return this.confidence() ? Math.round(this.confidence()! * 100) : 0;
+  });
+
+  constructor(private analysis: AnalysisService) {}
+
+  ngOnInit() {
+    this.analysis.result$.subscribe(res => {
+      if (res) {
+        this.confidence.set(res.confidence);
+        this.prediction.set(res.class_name);
+      }
+    });
+  }
+
+  labelMap: Record<string, string> = {
+    BKL: 'Queratosis benigna',
+    MEL: 'Melanoma',
+    NV: 'Nevo melanocítico',
+    BCC: 'Carcinoma basocelular',
+    AK: 'Queratosis actínica',
+    DF: 'Dermatofibroma',
+    VASC: 'Lesión vascular',
+    SCC: 'Carcinoma escamoso'
+  };
+
+  fullLabel = computed(() => {
+    return this.labelMap[this.prediction() ?? ''] || this.prediction();
+  });
+}
